d51c34ff46361637f8783b54dd988437395a3bea
Tobias Leinß <tobias.leinss@mway.io>
2023-05-20 23:35:04 +0200

    WIP contracts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,58 @@
 import { Box, Button, Center, Flex, Input, InputGroup, InputRightAddon, Text } from '@chakra-ui/react'
 import { Head } from 'components/layout/Head'
 import { HeadingComponent } from 'components/layout/HeadingComponent'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { SITE_DESCRIPTION } from 'utils/config'
-import { registryContract } from 'utils/contracts'
+import { stealthRegistryContract } from 'utils/contracts'
 import { LocalStorageKey } from 'utils/localstorage'
 import { generateStealthMetaAddress, utf8ToHex } from 'utils/web3'
-import { useAccount, useSignMessage } from 'wagmi'
+import { useAccount, useSignMessage, useWalletClient } from 'wagmi'
 
 export default function Home() {
   const [stealthPin, setStealthPin] = useState('')
   const [pinInput, setPinInput] = useState('')
   const [ethAmount, setEthAmount] = useState('')
-  const [stealthMetaAddress, setStealthMetaAddress] = useState('')
+  const [stealthMetaInfo, setStealthMetaInfo] = useState('')
 
   const { address } = useAccount()
-  const { data, isError, isLoading, isSuccess, signMessageAsync } = useSignMessage()
+  const { data: walletClient } = useWalletClient()
+  const { signMessageAsync } = useSignMessage()
 
   useEffect(() => {
     const stealthPin = localStorage.getItem(LocalStorageKey.StealthPin)
     if (stealthPin) setStealthPin(stealthPin)
+
+    const stealthInfo = localStorage.getItem(LocalStorageKey.StealthMetaInfoAddress)
+    if (stealthInfo) setStealthMetaInfo(JSON.parse(stealthInfo))
   }, [])
 
   async function onUsePin() {
-    if (!pinInput || !address) return
+    if (!pinInput || !address || !walletClient) return
 
-    const hex = '0x' + utf8ToHex(pinInput).slice(2)
-    const signedTx = await signMessageAsync({ message: hex })
+    try {
+      const hex = '0x' + utf8ToHex(pinInput).slice(2)
+      const signedTx = await signMessageAsync({ message: hex })
 
-    console.log('utf8ToHex(pinInput)', utf8ToHex(pinInput))
-    console.log('signedTx', signedTx)
+      console.log('utf8ToHex(pinInput)', utf8ToHex(pinInput))
+      console.log('signedTx', signedTx)
 
-    const { spendingPrivateKey, viewingPrivateKey, spendingPublicKey, viewingPublicKey, stealthMetaAddress } = generateStealthMetaAddress(signedTx)
+      const stealthInfo = generateStealthMetaAddress(signedTx)
 
-    console.log({ spendingPrivateKey, viewingPrivateKey, spendingPublicKey, viewingPublicKey, stealthMetaAddress })
+      await stealthRegistryContract.connect(walletClient).registerKey(stealthInfo.spendingPublicKey, stealthInfo.viewingPublicKey)
 
-    await registryContract.register(address, spendingPublicKey, viewingPublicKey)
+      localStorage.setItem(LocalStorageKey.StealthPin, pinInput)
+      localStorage.setItem(LocalStorageKey.StealthMetaInfoAddress, JSON.stringify(stealthInfo))
 
-    localStorage.setItem(LocalStorageKey.StealthPin, pinInput)
-    setStealthPin(pinInput)
-  }
-
-  const calledRegistry = useRef('')
-  useEffect(() => {
-    if (address && calledRegistry.current !== address) {
-      calledRegistry.current = address
-      ;(async () => {
-        const stealthAddress = await registryContract.getMeta(address)
-        if (stealthAddress) setStealthMetaAddress(stealthAddress)
-      })()
+      setStealthPin(pinInput)
+      setStealthMetaInfo(stealthMetaInfo)
+    } catch (err: any) {
+      console.error(`Error onUsePin: ${err.message}`)
     }
-  }, [address])
-
-  // const prevAddress = useRef(address)
-  // useEffect(() => {
-  //   if (prevAddress.current && !address) {
-  //     return onResetPin()
-  //   }
-
-  //   prevAddress.current = address
-  // }, [address])
+  }
 
   function onResetPin() {
     localStorage.removeItem(LocalStorageKey.StealthPin)
+    localStorage.removeItem(LocalStorageKey.StealthMetaInfoAddress)
     setStealthPin('')
     setPinInput('')
   }
@@ -79,15 +69,18 @@ export default function Home() {
 
       <main>
         <HeadingComponent as="h2">{SITE_DESCRIPTION}</HeadingComponent>
-
         {!address && (
           <Box mx="auto" width={'50vw'} mt={8}>
             <Center>Please log in with your wallet 🪪</Center>
           </Box>
         )}
-
+        {address && (
+          <Box mt={8}>
+            <Text>Receiver actions</Text>
+          </Box>
+        )}
         {address && !stealthPin && (
-          <Box mx="auto" width={'50vw'} mt={8}>
+          <Box mx="auto" width={'50vw'} mt={4}>
             <Text>No setup found. Start with a pin but pls don&apos;t forget it 👀</Text>
             <InputGroup mt={4}>
               <Input type="number" placeholder="Enter Pin" value={pinInput} onChange={(e) => setPinInput(e.target.value)} />
@@ -99,25 +92,27 @@ export default function Home() {
             </Button>
           </Box>
         )}
-
         {address && stealthPin && (
           <Box mx="auto" width={'50vw'} mt={8}>
-            <InputGroup>
+            <Button colorScheme="gray" onClick={onResetPin}>
+              Reset Pin
+            </Button>
+            <Box mt={8}>
+              <Text>Sender actions</Text>
+            </Box>
+
+            <InputGroup mt={8}>
               <Input type="number" placeholder="Eth Amount" value={ethAmount} onChange={(e) => setEthAmount(e.target.value)} />
               <InputRightAddon>ETH</InputRightAddon>
             </InputGroup>
-
             <Flex justifyContent={'space-between'} mt={4}>
               <Button colorScheme="blue" onClick={onAnonEth}>
                 Anonymize Eth
               </Button>
-              <Button colorScheme="gray" onClick={onResetPin}>
-                Reset Pin
-              </Button>
             </Flex>
           </Box>
         )}
-
+        ƒ
         {/* <Text py={4}>
           <LinkComponent href="examples">View examples</LinkComponent> to bootstrap development.
         </Text> */}
--- src/utils/localstorage.ts
+++ src/utils/localstorage.ts
@@ -1,3 +1,4 @@
 export enum LocalStorageKey {
   StealthPin = 'stealth_pin',
+  StealthMetaInfoAddress = 'stealth_meta_info',
 }
