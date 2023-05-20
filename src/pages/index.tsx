01f7e0fd970bcd7cea6593f330bc1a45e8a21041
Tobias Leinß <tobias.leinss@mway.io>
2023-05-20 13:29:40 +0200

    Add pin to page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,27 +4,67 @@ import { HeadingComponent } from 'components/layout/HeadingComponent'
 import { LinkComponent } from 'components/layout/LinkComponent'
 import { SITE_DESCRIPTION } from 'utils/config'
 import { Input } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { LocalStorageKey } from 'utils/localstorage'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 export default function Home() {
+  const [stealthPin, setStealthPin] = useState('')
+  const [pinInput, setPinInput] = useState('')
+
+  useEffect(() => {
+    const stealthPin = localStorage.getItem(LocalStorageKey.StealthPin)
+    if (stealthPin) setStealthPin(stealthPin)
+  }, [])
+
+  function onUsePin() {
+    if (!pinInput) return
+
+    localStorage.setItem(LocalStorageKey.StealthPin, pinInput)
+    setStealthPin(pinInput)
+  }
+
+  function onResetPin() {
+    localStorage.removeItem(LocalStorageKey.StealthPin)
+    setStealthPin('')
+    setPinInput('')
+  }
+
   return (
     <>
       <Head />
 
       <main>
         <HeadingComponent as="h2">{SITE_DESCRIPTION}</HeadingComponent>
-        <Text>Quickly ship Web3 Apps ⚡</Text>
 
-        <Box mx="auto" width={'50vw'}>
-          <InputGroup>
-            <Input type="number" placeholder="Send Eth" />
-            <InputRightAddon>ETH</InputRightAddon>
-          </InputGroup>
-          {/* <Input placeholder="Send Eth" /> */}
-        </Box>
+        {!stealthPin && (
+          <Box mx="auto" width={'50vw'} mt={8}>
+            <Text>No setup found. Start with a pin but pls don&apos;t forget it 👀</Text>
+            <InputGroup mt={4}>
+              <Input type="number" placeholder="Enter Pin" value={pinInput} onChange={(e) => setPinInput(e.target.value)} />
+              <InputRightAddon>#</InputRightAddon>
+            </InputGroup>
+            <Button colorScheme="blue" mt={4} onClick={onUsePin}>
+              Use Pin
+            </Button>
+          </Box>
+        )}
 
-        <Text py={4}>
+        {stealthPin && (
+          <Box mx="auto" width={'50vw'} mt={8}>
+            <InputGroup>
+              <Input type="number" placeholder="Send Eth" />
+              <InputRightAddon>ETH</InputRightAddon>
+            </InputGroup>
+            <Button colorScheme="blue" mt={4} onClick={onResetPin}>
+              Reset Pin
+            </Button>
+          </Box>
+        )}
+
+        {/* <Text py={4}>
           <LinkComponent href="examples">View examples</LinkComponent> to bootstrap development.
-        </Text>
+        </Text> */}
       </main>
     </>
   )
---
+++ src/utils/localstorage.ts
@@ -0,0 +1,3 @@
+export enum LocalStorageKey {
+  StealthPin = 'stealth_pin',
+}
