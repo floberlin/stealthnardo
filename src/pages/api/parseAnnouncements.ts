9674a1b22c773a946ca833f64c97f9a8dfc7380d
Tobias Leinß <tobias.leinss@mway.io>
2023-05-21 11:30:56 +0200

    WIP parseAnnouncements

---
+++ src/pages/api/parseAnnouncements.ts
@@ -0,0 +1,53 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { ethers } from 'ethers'
+import { stealthMessengerContract } from 'utils/contracts'
+import { SCROLL_CHAIN } from 'utils/config'
+import optimisticParsing from 'utils/stealth/optimisticParsing'
+import privToAddress from 'utils/stealth/privToAddress'
+
+const fromBlock = 2285931 // contract deployment
+const toBlock = 'latest'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { providedEmphemeralKey, spendingPublicKey, viewingPrivateKey, spendingPrivateKey } = req.body
+  if (!providedEmphemeralKey || !spendingPublicKey || !viewingPrivateKey || !spendingPrivateKey) {
+    res.status(400).json({ message: 'Missing required fields' })
+    return
+  }
+
+  const events = await stealthMessengerContract
+    .connect(new ethers.providers.JsonRpcProvider(SCROLL_CHAIN.rpcUrls.default.http[0]))
+    .queryFilter('Announcement', fromBlock, toBlock)
+
+  for (const event of events) {
+    parseAnnouncement(providedEmphemeralKey, spendingPublicKey, viewingPrivateKey, spendingPrivateKey)
+  }
+
+  res.status(200).json({ name: 'John Doe' })
+}
+
+function parseAnnouncement(providedEmphemeralKey: string, spendingPublicKey: string, viewingPrivateKey: string, spendingPrivateKey: string) {
+  try {
+    const stealthInfo = optimisticParsing(providedEmphemeralKey, spendingPublicKey, viewingPrivateKey)
+
+    const stealthAddress = stealthInfo[0]
+    const ephemeralPublicKey = stealthInfo[1]
+    const hashedSharedSecret = stealthInfo[2]
+
+    const stealthPrivateKey =
+      (BigInt(spendingPrivateKey) + BigInt(hashedSharedSecret)) % BigInt('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141')
+
+    let stealthPrivateKeyString = stealthPrivateKey.toString(16)
+    stealthPrivateKeyString = '0x' + stealthPrivateKeyString.padStart(64, '0')
+
+    const parsedStealthAddress = privToAddress(stealthPrivateKey)
+
+    if (stealthAddress !== parsedStealthAddress) {
+      return false
+    }
+
+    return true
+  } catch (err) {
+    console.error(err)
+  }
+}
--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -1,7 +1,11 @@
 import { ethers } from 'ethers'
-import stealthRegistryArtifact from '../../contracts/out/StealthnardoRegistry.sol/StealthRegistry.json'
+import stealthEscrowArtifact from '../../contracts/out/Escrow.sol/PublicStealthEscrow.json'
+import stealthMessengerArtifact from '../../contracts/out/Messenger.sol/IERC5564Messenger.json'
 import stealthHandlerArtifact from '../../contracts/out/StealthnardoHandler.sol/StealthHandler.json'
-import { stealthRegistry, stealthHandler } from './address'
+import stealthRegistryArtifact from '../../contracts/out/StealthnardoRegistry.sol/StealthRegistry.json'
+import { stealthEscrow, stealthHandler, stealthMessenger, stealthRegistry } from './address'
 
+export const stealthMessengerContract = new ethers.Contract(stealthMessenger, stealthMessengerArtifact.abi)
+export const stealthEscrowContract = new ethers.Contract(stealthEscrow, stealthEscrowArtifact.abi)
 export const stealthRegistryContract = new ethers.Contract(stealthRegistry, stealthRegistryArtifact.abi)
 export const stealthHandlerContract = new ethers.Contract(stealthHandler, stealthHandlerArtifact.abi)
